4d9c0ec8ea2a22aebfece974f1bf6ec6c95f40bc
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-09 19:08:40 +0200

    Add insert(data) method to Tree class.

--- Tree.js
+++ Tree.js
@@ -3,10 +3,12 @@ import Node from './Node.js'
 export default class Tree{
     #root = null;
 
-    constructor(array){
+    constructor(array=[]){
         this.#root = this.#buildTree(array);
     }
 
+    // Function that takes an array of data and turns it into a balanced 
+    // binary tree full of Node objects appropriately placed
     #buildTree(array){
         // remove duplicates (to simplify the tree balancing)
         array = array.filter((val, idx, arr) => arr.indexOf(val) === idx);
@@ -16,6 +18,7 @@ export default class Tree{
         // however, you can expect O(n log(n)) if using Mergesort	
         array.sort((a,b) => a-b);
 
+        // the BSC is built in O(n) using the sorted array data
         return this.#buildTreeFromSortedArray(array,0,array.length-1);
     }
 
@@ -30,6 +33,28 @@ export default class Tree{
         root.right = this.#buildTreeFromSortedArray(array,mid+1,end);
 
         return root;
+    }
+
+    // insert the given value
+    // such value is inserted as a leaf in the tree, 
+    // so that the tree is not rearranged
+    #insert(data,node = this.#root){
+        if (node.data === data)
+            return;
+
+        let side = node.data > data ? 'left' : 'right';
+        if (node[side]){
+            this.#insert(data, node[side]);
+        } else{
+            node[side] = new Node(data);
+        }
+    }
+    insert(data){
+        if (this.#root===null){
+            this.#root = new Node(data);
+        } else {
+            this.#insert(data, this.#root);
+        }
     }
 
     // The following method has been adapted from the function in 
--- testTree.js
+++ testTree.js
@@ -6,3 +6,23 @@ let tree = new Tree(array);
 console.log('Array:',array);
 console.log('Balanced Search Tree:');
 tree.prettyPrint();
+
+let dataToInsert = [2,7,323,24];
+for (let data of dataToInsert){
+    console.log(`\nInserting value ${data}`);
+    tree.insert(data);
+    tree.prettyPrint();
+}
+
+// ---------------------------------------------------------------------
+
+let emptyTree = new Tree([]);
+
+console.log('Balanced Search Tree (empty):');
+emptyTree.prettyPrint();
+
+for (let data of dataToInsert){
+    console.log(`\nInserting value ${data}`);
+    emptyTree.insert(data);
+    emptyTree.prettyPrint();
+}
