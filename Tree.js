bbf6ca4ff708b65c84f1050cc31f978e83377631
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-09 16:25:06 +0200

    Add Tree class
    The Tree class has been setup. The buildTree(array) method is
    currently just a dummy function, which will be implemented next.

---
+++ Tree.js
@@ -0,0 +1,17 @@
+import Node from './Node.js'
+
+export default Tree{
+    #root = null;
+
+    constructor(array){
+        this.#root = this.#buildTree(array);
+    }
+
+    #buildTree(array){
+        let root = null;
+
+        // todo
+
+        return root;
+    }
+}
