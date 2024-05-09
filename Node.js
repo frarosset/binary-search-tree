3c7ca645e79a976347f31a0827a1e1680b5f4f85
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-09 16:20:35 +0200

    Add Node class

---
+++ Node.js
@@ -0,0 +1,32 @@
+export default class Node{
+    #data = null;
+    #left = null;  // left child node
+    #right = null; // right child node
+
+    constructor(data=null, left=null, right=null){
+        this.#data = data;
+        this.#left = left;
+        this.#right = right;
+    }
+
+    set data(value){
+        this.#data = value;
+    }
+    get data(){
+        return this.#data;
+    }
+
+    set left(value){
+        this.#left = value;
+    }
+    get left(){
+        return this.#left;
+    }
+
+    set right(value){
+        this.#right = value;
+    }
+    get right(){
+        return this.#right;
+    }
+}
---
+++ testNode.js
@@ -0,0 +1,28 @@
+import Node from "./Node.js"
+
+let nodeLeft = new Node('L');
+let nodeRight = new Node('R');
+
+let nodeRoot = new Node('N',nodeLeft, nodeRight);
+let printNode = (node) => node ? node.data : node;
+
+console.log('Root node: ', printNode(nodeRoot));
+console.log('-Left node: ', printNode(nodeRoot.left));
+console.log('--Left node: ', printNode(nodeRoot.left.left));
+console.log('--Right node: ', printNode(nodeRoot.left.right));
+console.log('-Right node: ', printNode(nodeRoot.right));
+console.log('--Left node: ', printNode(nodeRoot.right.left));
+console.log('--Right node: ', printNode(nodeRoot.right.right));
+
+console.log('\nSwapping left right nodes on root');
+[nodeRoot.left,nodeRoot.right] = [nodeRoot.right, nodeRoot.left];
+nodeRoot.data = 'R swapped';
+
+console.log('Root node: ', printNode(nodeRoot));
+console.log('-Left node: ', printNode(nodeRoot.left));
+console.log('--Left node: ', printNode(nodeRoot.left.left));
+console.log('--Right node: ', printNode(nodeRoot.left.right));
+console.log('-Right node: ', printNode(nodeRoot.right));
+console.log('--Left node: ', printNode(nodeRoot.right.left));
+console.log('--Right node: ', printNode(nodeRoot.right.right));
+
