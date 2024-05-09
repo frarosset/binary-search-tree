84c7bdc23fb644e87f0b44ae14d9a3af8efc4fbd
frarosset <37275581+frarosset@users.noreply.github.com>
2024-05-09 18:12:59 +0200

    Implement buildTree() method of Tree class
    The buildTree() method is now complete.
    After preprocessing the input array into a sorted array with unique
    values, it builds a balanced tree in O(n).
    
    This is done by the helper method buildTreeFromSortedArray() of the
    Tree class.
    
    The function prettyPrint() from:
    https://www.theodinproject.com/lessons/javascript-binary-search-trees
    has been adapted into a method.
    To print the tree, just use tree.prettyPrint(), without arguments.

--- Tree.js
+++ Tree.js
@@ -8,21 +8,42 @@ export default class Tree{
     }
 
     #buildTree(array){
-        let root = null;
-
-        console.log(array)
         // remove duplicates (to simplify the tree balancing)
         array = array.filter((val, idx, arr) => arr.indexOf(val) === idx);
-        console.log(array)
         
         // sort the array using the sort() method
         // its complexity depends on the implementation
         // however, you can expect O(n log(n)) if using Mergesort	
         array.sort((a,b) => a-b);
-        console.log(array);
 
-        // todo
+        return this.#buildTreeFromSortedArray(array,0,array.length-1);
+    }
+
+    #buildTreeFromSortedArray(array,start,end){
+        if (start > end)
+            return null;
+
+        let mid = Math.ceil((start + end) / 2);
+
+        let root = new Node(array[mid]);
+        root.left = this.#buildTreeFromSortedArray(array,start,mid-1);
+        root.right = this.#buildTreeFromSortedArray(array,mid+1,end);
 
         return root;
+    }
+
+    // The following method has been adapted from the function in 
+    // https://www.theodinproject.com/lessons/javascript-binary-search-trees
+    prettyPrint(node = this.#root, prefix = "", isLeft = true){
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
     }
 }
--- testTree.js
+++ testTree.js
@@ -2,3 +2,7 @@ import Tree from './Tree.js'
 
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let tree = new Tree(array);
+
+console.log('Array:',array);
+console.log('Balanced Search Tree:');
+tree.prettyPrint();
